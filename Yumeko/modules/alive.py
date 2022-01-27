eddb848f4865752907de72493a760285912e2403
XhateXd <98488953+XhateXd@users.noreply.github.com>
2022-01-27 23:53:55 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -7,17 +7,17 @@ from pyrogram import __version__ as pyrover
 from Yumeko.events import register as MEMEK
 from Yumeko import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/90b49303716084ccb7f98.mp4"
+PHOTO = "https://telegra.ph/file/b58f003c6401cbf0e5399.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
   YUMEKO = "**Hello I'm Enmu!** \n\n"
   YUMEKO += "×**I'm Working Properly** \n\n"
-  YUMEKO += "×**My Owners : [𝐊𝐨𝐤𝐮𝐬𝐡𝐢𝐛𝐨](https://t.me/UppermoonX1), [Muzan](https://t.me/Demon_xLord)** \n\n"
+  YUMEKO += "×**My Owners : [𝐊𝐨𝐤𝐮𝐬𝐡𝐢𝐛𝐨]](https://t.me/UppermoonX1), [Muzan](https://t.me/Demon_xLord)** \n\n"
   YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
-  YUMEKO += "**Thanks For Adding Me Here ❤️**"
+  YUMEKO += "**Thanks For Summoning Me Here 🖤**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/enmu_kizuki_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/enmu_chat_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=ENMU,  buttons=BUTTON)
 
