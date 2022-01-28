54ae4965fe0dba588277349fa2af2a75cb5586f9
XhateXd <98488953+XhateXd@users.noreply.github.com>
2022-01-28 23:50:08 +0530

    Update reporting.py

--- Yumeko/modules/reporting.py
+++ Yumeko/modules/reporting.py
@@ -94,7 +94,7 @@ def report(update: Update, context: CallbackContext) -> str:
             return ""
 
         if reported_user.id in REPORT_IMMUNE_USERS:
-            message.reply_text("Uh? You reporting a disaster?")
+            message.reply_text("Uh? You reporting a Kizuki?")
             return ""
 
         if chat.username and chat.type == Chat.SUPERGROUP:
