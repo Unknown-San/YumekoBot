6661d432c36d8732423b22d5f56584b75e3bd822
XhateXd <98488953+XhateXd@users.noreply.github.com>
2022-01-29 02:43:11 +0530

    Update antiflood.py (#11)

--- Yumeko/modules/antiflood.py
+++ Yumeko/modules/antiflood.py
@@ -85,7 +85,7 @@ def check_flood(update, context) -> str:
             execstrings = "Muted for {}".format(getvalue)
             tag = "TMUTE"
         send_message(
-            update.effective_message, "Beep Boop! Boop Beep!\n{}!".format(execstrings)
+            update.effective_message, "Beep Boop! Enmu's Magic!\n{}!".format(execstrings)
         )
 
         return (
