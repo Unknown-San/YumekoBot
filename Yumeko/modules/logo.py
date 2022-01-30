9985c2bff24520c5a57ff7400ecedd958e5b069e
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-01-29 19:42:19 -0800

    Update logo.py

--- Yumeko/modules/logo.py
+++ Yumeko/modules/logo.py
@@ -268,7 +268,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./Yumeko/logo/*")
+    fnt = glob.glob("./Yumeko/logopom/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 120)
     w, h = draw.textsize(text, font=font)
